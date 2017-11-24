7854882a7f01394307f89372ff4f0c67b73c6441
Arthur Crippa Búrigo <arthurcburigo@gmail.com>
2017-11-24 14:13:51 -0200

    Adding interpretation of OGF and OCF in CommandPacket.

--- README.md
+++ README.md
@@ -16,12 +16,14 @@ In [2]: pkt = hci.command.ATT_ExchangeMTUReq(conn_handle=0x0000, client_rx_mtu=1
 In [3]: print(pkt)
 Packet Type: 0x1 (COMMAND)
 OpCode: 0xfd02 (ATT_EXCHANGE_MTU_REQUEST)
+  OGF: 0x3f (VENDOR_SPECIFIC)
+  OCF: 0x102 (258)
 Data Length: 0x4 (4)
 Connection Handle: 0x0 (0)
 Client RX MTU: 0x64 (100)
 
-In [4]: pkt.binary
-Out[4]: b'\x01\x02\xfd\x04\x00\x00d\x00'
+In [4]: print(pkt.binary)
+b'\x01\x02\xfd\x04\x00\x00d\x00'
 ```
 
 ### Parse binary packet:
@@ -34,6 +36,8 @@ In [2]: pkts = hci.from_binary(b'\x01\x02\xfd\x04\x00\x00d\x00')
 In [3]: print(pkts[0])
 Packet Type: 0x1 (COMMAND)
 OpCode: 0xfd02 (ATT_EXCHANGE_MTU_REQUEST)
+  OGF: 0x3f (VENDOR_SPECIFIC)
+  OCF: 0x102 (258)
 Data Length: 0x4 (4)
 Connection Handle: 0x0 (0)
 Client RX MTU: 0x64 (100)
--- hci/command/command_packet.py
+++ hci/command/command_packet.py
@@ -1,4 +1,5 @@
 from struct import pack, unpack
+from enum import IntEnum
 
 from ..hci_packet import HciPacket
 from .opcode import OpCode
@@ -7,6 +8,16 @@ from .opcode import OpCode
 class CommandPacket(HciPacket):
     OFFSET_DATA_LENGTH = 0x03
     OpCode = OpCode
+
+    class Ogf(IntEnum):
+        LINK_CONTROL = 1
+        LINK_POLICY = 2
+        CONTROLLER_AND_BASEBAND = 3
+        INFORMATIONAL_PARAMETERS = 4
+        STATUS_PARAMETERS = 5
+        TESTING = 6
+        LE_CONTROLLER = 8
+        VENDOR_SPECIFIC = 63
 
     def __init__(self, opcode, parameters=b''):
         super().__init__(
@@ -26,6 +37,14 @@ class CommandPacket(HciPacket):
         return unpack('<H', data)[0]
 
     @property
+    def ogf(self):
+        return (self.opcode >> 10)
+
+    @property
+    def ocf(self):
+        return self.opcode & 0x3FF
+
+    @property
     def parameter_total_length(self):
         OFFSET, SIZE_OCTETS = 3, 1
         data = self._get_data(OFFSET, SIZE_OCTETS)
@@ -34,10 +53,16 @@ class CommandPacket(HciPacket):
     def __str__(self):
         return super().__str__() + '\n' + '\n'.join([
             'OpCode: {} ({})',
+            '  OGF: {} ({})',
+            '  OCF: {} ({})',
             'Data Length: {} ({})',
         ]).format(
             hex(self.opcode),
             OpCode(self.opcode).name,
+            hex(self.ogf),
+            CommandPacket.Ogf(self.ogf).name,
+            hex(self.ocf),
+            int(self.ocf),
             hex(self.parameter_total_length),
             int(self.parameter_total_length),
         )
