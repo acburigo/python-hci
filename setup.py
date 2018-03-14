2e363909a265c09ee2f982ef68bdeb9c158c6c80
Arthur Crippa Búrigo <arthurcburigo@gmail.com>
2018-03-14 17:16:54 -0300

    Version 0.0.8.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='hci',
-    version='0.0.7',
+    version='0.0.8',
     description='Library for creating and parsing HCI packets.',
     url='https://github.com/acburigo/python-hci',
     license='MIT',
